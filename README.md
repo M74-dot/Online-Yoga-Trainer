77c105e27c44863f0ad9f62719bbe2888da1e031
manishamali1525 <manishamali1525@users.noreply.github.com>
2023-05-29 11:32:08 +0530

    added README.md file

--- README.md
+++ README.md
@@ -2,5 +2,32 @@
 
 ## How to run this application
 
+### Python version required : Python 3.9.6 or greater
+
 ### firstly clone the repository
-> 
+> git clone https://github.com/M74-dot/Online-Yoga-Trainer.git
+
+### Run requirements.txt file to get all modules installed
+> pip install -r requirements.txt
+
+### Run the application
+> streamlit run app.py
+
+### If after executing above command its saying that module not found you can install that module using command
+> pip install <module_name>
+
+### It will run on some port 
+
+### If want to run without sound simply comment out below lines of code from main.py file which is defined inside compare_pose function.
+``` # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the right arm at elbow")
+        engine.runAndWait()
+        time.sleep(5)
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+```
+
+THANK YOU!
--- main.py
+++ main.py
@@ -17,13 +17,12 @@ import base64
 from streamlit_image_select import image_select
 import hydralit_components as hc
 from PIL import Image
-# new addon
 import pyttsx3
 import time
 
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-
 
 
 # calculating angle between 3 points, within 0 to 180 degrees.   
@@ -232,8 +231,6 @@ def extractKeypoint(path):
 
                                      )          
 
-            #cv2.imshow('MediaPipe Feed',image)
-
             if cv2.waitKey(0) & 0xFF == ord('q'):
                 break
             
@@ -251,131 +248,239 @@ def compare_pose(image,angle_point,angle_user, angle_target ):
     cv2.putText(image, str("Score:"), (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
     height, width, _ = image.shape   
 
+
     if angle_user[0] < (angle_target[0] - 15):
-        #print("Extend the right arm at elbow")
         stage = stage + 1
         speak_instruction("Extend the right arm at elbow")
         cv2.putText(image, str("Extend the right arm at elbow"), (10,60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[0][0]*width), int(angle_point[0][1]*height)),30,(0,0,255),5) 
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the right arm at elbow")
+        engine.runAndWait()
+        time.sleep(5)
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
 
     if angle_user[0] > (angle_target[0] + 15):
-        #print("Fold the right arm at elbow")
         stage = stage + 1
         cv2.putText(image, str("Fold the right arm at elbow"), (10,80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[0][0]*width), int(angle_point[0][1]*height)),30,(0,0,255),5)
-        
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Fold the right arm at elbow")
+        engine.runAndWait()
+        # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
+     
     if angle_user[1] < (angle_target[1] -15):
-        #print("Extend the left arm at elbow")
         stage = stage + 1
         cv2.putText(image, str("Extend the left arm at elbow"), (10,100), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[1][0]*width), int(angle_point[1][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the left arm at elbow")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
 
     if angle_user[1] >(angle_target[1] + 15):
-        #print("Fold the left arm at elbow")
         stage = stage + 1
         cv2.putText(image, str("Fold the left arm at elbow"), (10,120), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[1][0]*width), int(angle_point[1][1]*height)),30,(0,0,255),5)
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Fold left arm at elbow")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
         
-        
+
     if angle_user[2] < (angle_target[2] - 15):
-        #print("Lift your right arm")
         stage = stage + 1
         cv2.putText(image, str("Lift your right arm"), (10,140), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[2][0]*width), int(angle_point[2][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Lift your right arm")
+        engine.runAndWait()
+        time.sleep(10)
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
 
     if angle_user[2] > (angle_target[2] + 15):
-        #print("Put your arm down a little")
         stage = stage + 1
         cv2.putText(image, str("Put your arm down a little"), (10,160), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[2][0]*width), int(angle_point[2][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Put your arm down a little")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
 
     if angle_user[3] < (angle_target[3] - 15):
-        #print("Lift your left arm")
         stage = stage + 1
         cv2.putText(image, str("Lift your left arm"), (10,180), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[3][0]*width), int(angle_point[3][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Lift your left arm")
+        engine.runAndWait()
+        time.sleep(10)
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
 
     if angle_user[3] > (angle_target[3] + 15):
-        #print("Put your arm down a little")
         stage = stage + 1
         cv2.putText(image, str("Put your arm down a little"), (10,200), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[3][0]*width), int(angle_point[3][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Put your arm down a little")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
         
 
     if angle_user[4] < (angle_target[4] - 15):
-        #print("Extend the angle at right hip")
         stage = stage + 1
         cv2.putText(image, str("Extend the angle at right hip"), (10,220), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[4][0]*width), int(angle_point[4][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the angle at right hip")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
+
 
     if angle_user[4] > (angle_target[4] + 15):
-        #print("Reduce the angle at right hip")
         stage = stage + 1
         cv2.putText(image, str("Reduce the angle of at right hip"), (10,240), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[4][0]*width), int(angle_point[4][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Reduce the angle of at right hip")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
 
 
     if angle_user[5] < (angle_target[5] - 15):
-        #print("Extend the angle at left hip")
         stage = stage + 1
         cv2.putText(image, str("Extend the angle at left hip"), (10,260), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[5][0]*width), int(angle_point[5][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the angle at left hip")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()  
 
 
     if angle_user[5] > (angle_target[5] + 15):
-        #print("Reduce the angle at left hip")
         stage = stage + 1
         cv2.putText(image, str("Reduce the angle at left hip"), (10,280), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[5][0]*width), int(angle_point[5][1]*height)),30,(0,0,255),5)
-         
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Reduce the angle at left hip")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()  
         
 
     if angle_user[6] < (angle_target[6] - 15):
-        #print("Extend the angle of right knee")
         stage = stage + 1
         cv2.putText(image, str("Extend the angle of right knee"), (10,300), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[6][0]*width), int(angle_point[6][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the angle of right knee")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
         
 
     if angle_user[6] > (angle_target[6] + 15):
-        #print("Reduce the angle of right knee")
         stage = stage + 1
         cv2.putText(image, str("Reduce the angle at right knee"), (10,320), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[6][0]*width), int(angle_point[6][1]*height)),30,(0,0,255),5)
-        
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Reduce the angle at right knee")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
        
     if angle_user[7] < (angle_target[7] - 15):
-        #print("Extend the angle at left knee")
         stage = stage + 1
         cv2.putText(image, str("Extend the angle at left knee"), (10,340), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[7][0]*width), int(angle_point[7][1]*height)),30,(0,0,255),5)
+         # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Extend the angle at left knee")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()       
        
-       
+
     if angle_user[7] > (angle_target[7] + 15):
-        #print("Reduce the angle at left knee")
         stage = stage + 1
         cv2.putText(image, str("Reduce the angle at left knee"), (10,360), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,153,0], 2, cv2.LINE_AA)
         cv2.circle(image,(int(angle_point[7][0]*width), int(angle_point[7][1]*height)),30,(0,0,255),5)
+        # Add text-to-speech conversion for this condition
+        engine = pyttsx3.init()
+        engine.say("Reduce the angle at left knee")
+        engine.runAndWait()
+         # Check for keyboard input to stop text-to-speech
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            engine.stop()
         
 
     if stage!=0:
-        #print("FIGHTING!")
         cv2.putText(image, str("Keep Trying!"), (170,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [0,0,255], 2, cv2.LINE_AA)
         
         pass
     else:
-        #print("PERFECT")
         cv2.putText(image, str("PERFECT"), (170,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [0,0,255], 2, cv2.LINE_AA)
         
 
@@ -389,7 +494,6 @@ def dif_compare(x,y):
         result = 1 - spatial.distance.cosine(list(x[i].values()),list(y[j].values()))
         average.append(result)
     score = math.sqrt(2*(1-round(Average(average),2)))
-    #print(Average(average))
     return score
 
 
@@ -398,8 +502,9 @@ def diff_compare_angle(x,y):
     for i,j in zip(range(len(x)),range(len(y))):
         z = np.abs(x[i] - y[j])/((x[i]+ y[j])/2)
         new_x.append(z)
-        #print(new_x[i])
     return Average(new_x)
+
+
 def convert_data(landmarks):
     df = pd.DataFrame(columns = ['x', 'y', 'z', 'vis'])
     for i in range(len(landmarks)):
@@ -418,8 +523,6 @@ def main(path):
     x = extractKeypoint(path)
     dim = (600, 400)
     resized = cv2.resize(x[3], dim, interpolation = cv2.INTER_AREA)
-
-    # cv2.imshow('target',resized)
     angle_target = x[2]
     point_target = x[1]
 
@@ -437,8 +540,8 @@ def main(path):
                 image_height, image_width, _ = image.shape
                 image = cv2.resize(image, (int(image_width * (860 / image_height)), 860))
                 
-                # added new
-                 # Display target frame on the top right of the screen
+            
+                # Display target frame on the top right of the screen
                 resized = cv2.resize(x[3], (300, 300))
                 image_height, image_width, _ = image.shape
                 image[0:300, image_width-300:image_width] = resized
@@ -545,5 +648,3 @@ def main(path):
 
             cap.release()
             cv2.destroyAllWindows()
-
-        
